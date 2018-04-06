ae251ab11b31bfc650170a0ec470e53897166aa3
ERIC KELLY <eakelly220@gmail.com>
2018-04-06 19:56:11 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -3,6 +3,9 @@ def position_taken?(board, index)
   if board[index] == " " ||  board[index] == ""
     return false 
   elsif board[index] == "X" ||  board[index] == "O"
+    return true
+  else 
+    return false
   end
   
 end
